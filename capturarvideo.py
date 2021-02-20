24a27ab3619f2f02453ec642a1c3f28de2ba0ef3
Tomas Jasso <62636466+xsrajasso@users.noreply.github.com>
2021-02-20 17:51:34 -0600

    1ra version
    primera version para el evento

---
+++ capturarvideo.py
@@ -0,0 +1,20 @@
+  
+import cv2
+
+captura = cv2.VideoCapture(0)
+ancho = 640
+alto = 480
+codigo = cv2.VideoWriter_fourcc(*'DIVX')
+salida = cv2.VideoWriter('Video.mp4', codigo, 20, (ancho, alto))
+
+while True:
+    ret, video = captura.read()
+    video = cv2.flip(video, 1)
+    salida.write(video)
+    cv2.imshow('Frame', video)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+captura.release()
+salida.release()
+cv2.destroyAllWindows()
